464992753d8204e2428bd74cad27340c05cf3962
Sebastian Farfan <sebas.654@gmail.com>
2023-07-01 01:56:13 -0300

    se creó un atributo "titulo" en el model para enviar el titulo de la pág.

--- Trabajo_Practico_Final/src/main/java/ar/edu/unju/fi/controller/IndexController.java
+++ Trabajo_Practico_Final/src/main/java/ar/edu/unju/fi/controller/IndexController.java
@@ -1,6 +1,7 @@
 package ar.edu.unju.fi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
 	@GetMapping ("/index")
-	public String getPaginaIndex() {
-	return "index";
+	public String getPaginaIndex(Model model) {
+		model.addAttribute("titulo", "Inicio");
+		return "index";
 	}
 }
