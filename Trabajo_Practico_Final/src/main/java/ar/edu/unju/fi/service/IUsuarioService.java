22c4777760b1da2f8ceeb1474a52fe1aacdb1409
NestorCorimayo <NestorCorimayo@DESKTOP-624CO4I>
2023-07-05 17:17:07 -0300

    se ha implementado la capa service de usuario con su respectiva documentacion, y la incorporacion de Autowirred y Override, y su respectiva interfaz

---
+++ Trabajo_Practico_Final/src/main/java/ar/edu/unju/fi/service/IUsuarioService.java
@@ -0,0 +1,48 @@
+package ar.edu.unju.fi.service;
+
+import java.util.List;
+import ar.edu.unju.fi.entity.Usuario;
+
+/**
+* Esta interfaz define los métodos para administrar los usuarios.
+*/
+public interface IUsuarioService {
+	
+	/**
+	* Obtiene la lista de todos los usuarios.
+	* @return una lista de usuarios.
+	*/
+	List<Usuario> getLista();
+	
+	/**
+	*  Guarda un nuevo usuario.
+	*  @param usuario: usuario a guardar.
+	*/
+	void guardar (Usuario usuario);
+	
+	/**
+	*  Modifica un usuario existente.
+	*  @param usuario: usuario a modificar.
+	*/
+	void modificar (Usuario usuario);
+	
+	/**
+	*  Elimina un usuario existente.
+	*  @param usuarioEncontrado: usuario a eliminar.
+	*/
+	void eliminar (Usuario usuarioEncontrado);
+	
+	/**
+	    * Encuentra un usuario de acuerdo a su id.
+	    * @param id: identificador del usuario a buscar.
+	    * @return el usuario que tenga coincidencia.
+	    */
+	Usuario getBy(Long id);
+	
+	/**
+	    * Obtiene un nuevo usuario.
+	    * @return un objeto usuario.
+	    */
+	Usuario getUsuario();
+	
+}
