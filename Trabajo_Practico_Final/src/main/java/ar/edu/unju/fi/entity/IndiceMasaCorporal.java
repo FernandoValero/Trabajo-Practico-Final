0710d6d6c04fdea8e1a2a842c8dfd7000e400edc
Sebastian Farfan <sebas.654@gmail.com>
2023-07-08 05:33:53 -0300

    se mdificó un validador en imc

--- Trabajo_Practico_Final/src/main/java/ar/edu/unju/fi/entity/IndiceMasaCorporal.java
+++ Trabajo_Practico_Final/src/main/java/ar/edu/unju/fi/entity/IndiceMasaCorporal.java
@@ -15,7 +15,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
 
 /**
  * Representa un Indice de Masa Corporal
