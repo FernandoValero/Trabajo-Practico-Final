7e868ca45cd0ea55b2e1d8c396967d40ca34a77c
David Martinez <David@DESKTOP-MHN5J54>
2023-07-09 17:34:30 -0300

    Se crea el archivo para crear usuarios administradores

---
+++ Trabajo_Practico_Final/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+-- Usuarios administradores por defecto
+INSERT INTO trabajofinal.usuarios (usu_admin,usu_estado, usu_estatura, usu_fecha_nacimineto, usu_id, usu_apellido, usu_email, usu_nombre, usu_sexo, usu_telefono) VALUES (1,1, 1.85, '1974-11-10', 1 , 'Valdez','vdejuan@empresa.com','Juan', 'M', '3884589546');
+INSERT INTO trabajofinal.usuarios (usu_admin,usu_estado, usu_estatura, usu_fecha_nacimineto, usu_id, usu_apellido, usu_email, usu_nombre, usu_sexo, usu_telefono) VALUES (1,1, 1.65, '1986-9-9', 2 , 'Perez','mercie@empresa.com','Merci', 'F', '3781247895');
