dfeb7b3dd0bf2d950ccf99b55e731611a841063a
eveli <eveli@DESKTOP-FMF8N0N>
2023-07-09 19:28:15 -0300

    Se realizó la documentación para archivo controller de registro

--- Trabajo_Practico_Final/src/main/java/ar/edu/unju/fi/controller/RegistroController.java
+++ Trabajo_Practico_Final/src/main/java/ar/edu/unju/fi/controller/RegistroController.java
@@ -18,69 +18,114 @@ import jakarta.validation.Valid;
 @RequestMapping("/registro")
 public class RegistroController {
 
-	@Autowired
-	// @Qualifier("usuarioServiceMysql")
-	private IUsuarioService usuarioService;
+    @Autowired
+    private IUsuarioService usuarioService;
 
-	@GetMapping("/formulario")
-	public String getPaginaRegistros(Model model, @RequestParam(value = "perfil", required = false) String perfil) {
-		Usuario usuario = usuarioService.getUsuario();
-		if (usuario != null && perfil != null && perfil.equals("Modificar")) {
-			model.addAttribute("edicion", true);
-			model.addAttribute("perfil", "Modificar");
-		} else {
-			model.addAttribute("edicion", false);
-			model.addAttribute("perfil", "Registrarme");
-		}
-		model.addAttribute("usuario", usuario);
-		return "registros";
-	}
+    /**
+     * Maneja la solicitud GET para mostrar el formulario de registro.
+     * Si se proporciona el parámetro "perfil" con el valor "Modificar", se cargan los datos del usuario para su edición.
+     * 
+     * @param model  el modelo utilizado para pasar datos a la vista
+     * @param perfil el perfil del usuario ("Modificar" para edición, o null para registro nuevo)
+     * @return la vista "registros" para el formulario de registro/edición
+     */
+    @GetMapping("/formulario")
+    public String getPaginaRegistros(Model model, @RequestParam(value = "perfil", required = false) String perfil) {
+        Usuario usuario = usuarioService.getUsuario();
+        if (usuario != null && perfil != null && perfil.equals("Modificar")) {
+            model.addAttribute("edicion", true);
+            model.addAttribute("perfil", "Modificar");
+        } else {
+            model.addAttribute("edicion", false);
+            model.addAttribute("perfil", "Registrarme");
+        }
+        model.addAttribute("usuario", usuario);
+        return "registros";
+    }
 
-	@PostMapping("/enviar")
-	public String enviarUsuario(@Valid @ModelAttribute("usuario") Usuario usuario, BindingResult result, Model model) {
-		if (result.hasErrors()) {
-			model.addAttribute("edicion", false);
-			return "registros";
-		}
-		usuarioService.guardar(usuario);
-		Long idUsuario = usuario.getId(); // Obtener el ID del usuario guardado
-		model.addAttribute("guardadoExitoso", true);
-		model.addAttribute("idUsuario", idUsuario); // Agregar el ID del usuario al modelo
-		return "registros";
-	}
+    /**
+     * Maneja la solicitud POST para enviar los datos del formulario de registro/edición.
+     * Si los datos son válidos, se guarda el usuario en la base de datos.
+     * 
+     * @param usuario el usuario obtenido del formulario
+     * @param result  el resultado del proceso de validación del formulario
+     * @param model   el modelo utilizado para pasar datos a la vista
+     * @return la vista "registros" si hay errores de validación, o la misma vista con un mensaje de guardado exitoso
+     */
+    @PostMapping("/enviar")
+    public String enviarUsuario(@Valid @ModelAttribute("usuario") Usuario usuario, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("edicion", false);
+            return "registros";
+        }
+        usuarioService.guardar(usuario);
+        Long idUsuario = usuario.getId();
+        model.addAttribute("guardadoExitoso", true);
+        model.addAttribute("idUsuario", idUsuario);
+        return "registros";
+    }
 
-	@GetMapping("/login")
-	public String getPaginaLogin() {
-		return "ingreso";
-	}
+    /**
+     * Maneja la solicitud GET para mostrar la página de inicio de sesión.
+     * 
+     * @return la vista "ingreso" para el inicio de sesión
+     */
+    @GetMapping("/login")
+    public String getPaginaLogin() {
+        return "ingreso";
+    }
 
-	@GetMapping("/buscar")
-	public String getBuscarUsuarioPage(Model model, @RequestParam(value = "id") Long id) {
-		Usuario usuarioBuscado = usuarioService.getBy(id);
-		if (usuarioBuscado != null) {
-			model.addAttribute("usuario", usuarioBuscado);
-			model.addAttribute("perfil", "Modificar");
-			return "registros";
-		}
-		return "ingreso";
-	}
+    /**
+     * Maneja la solicitud GET para buscar un usuario por su ID.
+     * Si se encuentra el usuario, se cargan sus datos en el formulario de registro para su edición.
+     * 
+     * @param model el modelo utilizado para pasar datos a la vista
+     * @param id    el ID del usuario a buscar
+     * @return la vista "registros" con los datos del usuario si se encuentra, o la vista "ingreso" si no se encuentra
+     */
+    @GetMapping("/buscar")
+    public String getBuscarUsuarioPage(Model model, @RequestParam(value = "id") Long id) {
+        Usuario usuarioBuscado = usuarioService.getBy(id);
+        if (usuarioBuscado != null) {
+            model.addAttribute("usuario", usuarioBuscado);
+            model.addAttribute("perfil", "Modificar");
+            return "registros";
+        }
+        return "ingreso";
+    }
 
-	@GetMapping("/editar/{id}")
-	public String getEditarSucursalPage(Model model, @ModelAttribute("usuario") Usuario usuario) {
-		boolean edicion = true;
-		model.addAttribute("edicion", edicion);
-		return "registro";
-	}
+    /**
+     * Maneja la solicitud GET para mostrar la página de edición de un usuario.
+     * 
+     * @param model   el modelo utilizado para pasar datos a la vista
+     * @param usuario el usuario a editar (pasado como un objeto ModelAttribute)
+     * @return la vista "registro" para la edición del usuario
+     */
+    @GetMapping("/editar/{id}")
+    public String getEditarSucursalPage(Model model, @ModelAttribute("usuario") Usuario usuario) {
+        boolean edicion = true;
+        model.addAttribute("edicion", edicion);
+        return "registro";
+    }
 
-	@PostMapping("/editar/{id}")
-	public String editarUsuario(@Valid @ModelAttribute("usuario") Usuario usuario, BindingResult result, Model model) {
-		if (result.hasErrors()) {
-			model.addAttribute("edicion", true);
-			return "registros";
-		}
-		usuarioService.guardar(usuario);
-		model.addAttribute("guardadoExitoso", true);
-		return "registros";
-	}
-
+    /**
+     * Maneja la solicitud POST para guardar los cambios realizados en la edición de un usuario.
+     * Si los datos son válidos, se actualiza el usuario en la base de datos.
+     * 
+     * @param usuario el usuario obtenido del formulario de edición
+     * @param result  el resultado del proceso de validación del formulario
+     * @param model   el modelo utilizado para pasar datos a la vista
+     * @return la vista "registros" si hay errores de validación, o la misma vista con un mensaje de guardado exitoso
+     */
+    @PostMapping("/editar/{id}")
+    public String editarUsuario(@Valid @ModelAttribute("usuario") Usuario usuario, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("edicion", true);
+            return "registros";
+        }
+        usuarioService.guardar(usuario);
+        model.addAttribute("guardadoExitoso", true);
+        return "registros";
+    }
 }
+
